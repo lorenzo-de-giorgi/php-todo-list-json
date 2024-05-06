3ced307c4534c1224125fac4a12a1f7af85dcad5
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-05-06 16:37:16 +0200

    doinfg addTask function with server.php

--- js/script.js
+++ js/script.js
@@ -4,9 +4,13 @@ createApp({
     data() {
         return {
             todo: [],
+            apiUrl: 'server.php',
+            lastId: null,
+            newTask: {
+                text: "",
+            },
             itemText: '',
-            done: '',
-            apiUrl: 'server.php'
+            done: ''
         }
     },
     methods: {
@@ -14,38 +18,21 @@ createApp({
             axios.get(this.apiUrl).then((res) => {
                 this.todo = res.data;
             })
-        },
-        toggleDone(id){
-            const item = this.todo.find((el) => {
-                return el.id === id;
-            })
-            console.log(item)
-            if(item){
-                item.done = !item.done
-            }
-        },
-        removeItem(id){
-            const i = this.todo.findIndex((el) => el.id === id);
-            if(i !== -1){
-                this.todo.splice(i, 1);
-            }
-        },
+        }, 
         addTodo(){
-            const newObj = {
-                id: null, //generare id
-                text: this.itemText, //inserire valore
-                done: false
-            }
-            let nextId = 0;
-            this.todo.forEach((el) => {
-                if(nextId < el.id){
-                    nextId = el.id
-                }
-            });
-            newObj.id = nextId + 1;
-            this.todo.unshift(newObj);
-            // todo.unshift(newObj);
-            this.itemText = '';
+            const task = { ...this.newTask };
+            this.newTask = {
+                text: "",
+            },
+            this.lastId += 1;
+            task.id = this.lastId;
+            this.todo.push(task);
+            const data = new FormData();
+            data.append('text', task.text)
+            axios.post(this.apiUrl, data).then((res) => {
+                console.log(res.data);
+                this.lastId = this.todo.length - 1;
+            })
         }
     },
     computed: {
--- server.php
+++ server.php
@@ -1,5 +1,21 @@
 <?php
     $todoJson = file_get_contents("js/data.json");
     // var_dump($todoJson);
+    $method = $_SERVER['REQUEST_METHOD'];
+    if($method !== 'GET'){
+        $todo = json_decode($todoJson, true);
+        if($method === 'POST'){
+            if(isset($_POST['id'])){
+                var_dump($_POST);
+                $todoTask = [
+                    'id' => (int)$_POST['id'],
+                    'text' => $_POST['text']
+                ];
+                $todo[] = $todoTask;
+            }
+        }
+        $todoJson = json_encode($todo, JSON_PRETTY_PRINT);
+        file_put_contents('/js/data.json', $todoJson);
+    }
     header('Content-Type: application/json');
     echo($todoJson);
